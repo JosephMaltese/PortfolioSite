87654cbdc092e73d87e7021fc63167595d12854f
JosephMaltese <josephmaltese34@gmail.com>
2025-03-29 19:05:16 -0400

    Removed Main Image

--- app/components/LandingContent.tsx
+++ app/components/LandingContent.tsx
@@ -1,28 +1,51 @@
 import React from 'react'
 
+// const LandingContent = () => {
+//   return (
+//     <>
+//         <div className="hero bg-base-200 min-h-screen">
+//             <div className="hero-content flex-col lg:flex-row-reverse">
+//                 <img
+//                 src="/selfPortrait.jpg"
+//                 className="w-[70%] h-auto max-w-[80%] rounded-lg shadow-2xl md:w-[30%] md:h-[50%]" />
+//                 <div>
+//                 <h1 className="text-5xl font-bold">Hi, I&apos;m <span className="text-sky-300">Joseph Maltese</span></h1>
+//                 <p className="py-6">
+//                   A second-year Computer Science (BASc) Co-op student at McMaster University with a passion for coding and software development. I love to learn and explore new technologies, with particular
+//                   interest in full-stack development and artificial intelligence. As a member of the Software Architecture team for McMaster
+//                   Start Coding, I help develop the STaBL.rocks web application in Haskell, which is used to teach programming fundamentals
+//                   to K-PhD students. I actively participate in hackathons, where I enjoy collaborating with others to solve challenging problems
+//                   and build innovative projects.
+//                 </p>
+//                 </div>
+//             </div>
+//         </div>
+    
+//     </>
+//   )
+// }
+
 const LandingContent = () => {
   return (
     <>
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
-                src="/selfPortrait.jpg"
-                className="w-[70%] h-auto max-w-[80%] rounded-lg shadow-2xl md:w-[30%] md:h-[50%]" />
-                <div>
-                <h1 className="text-5xl font-bold">Hi, I&apos;m <span className="text-sky-300">Joseph Maltese</span></h1>
-                <p className="py-6">
+            <div className="hero-content">
+              <div className='justify-center'>
+                <h1 className="text-3xl sm:text-5xl font-bold px-5">Hi, I&apos;m <span className="text-sky-300">Joseph Maltese</span></h1>
+                <p className="py-10 px-5">
                   A second-year Computer Science (BASc) Co-op student at McMaster University with a passion for coding and software development. I love to learn and explore new technologies, with particular
                   interest in full-stack development and artificial intelligence. As a member of the Software Architecture team for McMaster
                   Start Coding, I help develop the STaBL.rocks web application in Haskell, which is used to teach programming fundamentals
                   to K-PhD students. I actively participate in hackathons, where I enjoy collaborating with others to solve challenging problems
                   and build innovative projects.
                 </p>
-                </div>
+              </div>
             </div>
         </div>
     
     </>
   )
 }
+
 
 export default LandingContent
