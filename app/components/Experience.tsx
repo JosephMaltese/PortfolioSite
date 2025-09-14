28e6ab995b5b1cfc78e27d19fa4031704312fbc8
JosephMaltese <josephmaltese34@gmail.com>
2025-09-13 22:23:39 -0400

    updated content and added experience

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -12,15 +12,15 @@ const Experience = () => {
           {
             title: "2025", content: (
               <div>
-                <ExperienceItem organizationName='Fondation STaBL Foundation' role="Research & Development Intern" description='Contributing to the design and development of a software project focused on improving computing education, under the
-supervision of Dr. Christopher Anand.' date="April 2025 - August 2025" imageName='fondation_stabl_foundation_logo.jpeg'/>
+                <ExperienceItem organizationName='McMaster Start Coding | McMaster University' role="Jr. VP Software Architecture" description='Works alongside fellow executives to manage and organize the software architecture team, leading weekly scrum meetings, reviewing pull requests, driving project initiatives, and contributing to the full-stack development of the STaBL.rocks site.' date="September 2025 - Present" imageName='mcmasterStartCoding.jpg'/>
+                <ExperienceItem organizationName='Fondation STaBL Foundation' role="Research & Development Intern" description='Collaborated on Music Creator 2.0 under Dr. Christopher Anand (Department of Computing & Software, McMaster University), an accessible educational tool that teaches programming through music for visually impaired students. Contributed full-stack development using JavaScript for the frontend, Node.js for the backend, and PostgreSQL for data management.' date="April 2025 - August 2025" imageName='fondation_stabl_foundation_logo.jpeg'/>
                 <ExperienceItem organizationName='Google Developer Student Club | McMaster University' role="Web Developer - Marketing & Branding Team" description='Worked with a team of software developers and designers to build and maintain the club&apos;s new website for its 2025 rebranding. Used the Next.js framework to deliver a modern and performant web experience.' date="January 2025 - April 2025" imageName='gdsc_logo.png'/>
               </div>
             )
           },
           { title:"2024", content: (
             <div>
-              <ExperienceItem organizationName='McMaster Start Coding | McMaster University' role="Full-Stack Developer" description='Works with a team of volunteer software developers to maintain and develop the STaBL.rocks web application, which is used by McMaster Start Coding to teach K-PhD students the fundamentals of programming. Utilizes scrum methodologies. Uses the full-stack Haskell web framework, IHP.' date="September 2024-Present" imageName='mcmasterStartCoding.jpg'/>
+              <ExperienceItem organizationName='McMaster Start Coding | McMaster University' role="Full-Stack Developer" description='Works with a team of volunteer software developers to maintain and develop the STaBL.rocks web application, which is used by McMaster Start Coding to teach K-PhD students the fundamentals of programming. Utilizes scrum methodologies. Uses the full-stack Haskell web framework, IHP.' date="September 2024 - August 2025" imageName='mcmasterStartCoding.jpg'/>
               <ExperienceItem organizationName='HammerHacks | McMaster University' role="Technical Mentor" description='Helped students brainstorm creative project ideas which fit the theme of the hackathon. Gave suggestions for which technologies could help them achieve their goals. Assisted with any technical issues that students faced throughout the course of the competition.' date="November 23, 2024" imageName='hammerHacks.webp'/>
             </div>
           ) },
--- app/components/LandingContent.tsx
+++ app/components/LandingContent.tsx
@@ -8,7 +8,7 @@ const LandingContent = () => {
               <div className='justify-center'>
                 <h1 className="text-4xl sm:text-5xl font-bold px-5">Hi, I&apos;m <span className="text-sky-300">Joseph Maltese</span></h1>
                 <p className="py-10 px-5">
-                  An incoming third-year Computer Science (BASc) Co-op student at McMaster University with a passion for coding and software development. I love to learn and explore new technologies, with particular
+                  A third-year Computer Science (BASc) Co-op student at McMaster University with a passion for coding and software development. I love to learn and explore new technologies, with particular
                   interest in full-stack development and artificial intelligence. As a member of the Software Architecture team for McMaster
                   Start Coding, I help develop the STaBL.rocks web application in Haskell, which is used to teach programming fundamentals
                   to K-PhD students. I actively participate in hackathons, where I enjoy collaborating with others to solve challenging problems
